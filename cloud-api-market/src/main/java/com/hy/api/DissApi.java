f12dc1c769392c626d78321420685488a90c2703
putian <3048561573@qq.com>
2025-08-06 10:11:30 +0800

    8/6 ①完成两种流控效果的验证，warm up和排队等待 ②Sentinel支持的雪崩解决方案:     线程隔离(仓壁模式)     降级熔断 ③Feign整合Sentinel的步骤:     在application.yml中配置:feign.sentienl.enable=true。给FeignClient编写FallbackFactory并注册为Bean。将FallbackFactory配置到FeignClient

--- cloud-api-market/src/main/java/com/hy/api/DissApi.java
+++ cloud-api-market/src/main/java/com/hy/api/DissApi.java
@@ -1,5 +1,6 @@
 package com.hy.api;
 
+import com.hy.back.DissFallback;
 import com.hy.pojo.TbDistributor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient("cloud-diss-service")
+@FeignClient(name = "cloud-diss-service",fallbackFactory = DissFallback.class)
 public interface DissApi {
     //根据远程传递过来的id查询当前id用户所管理的分销商信息
     @GetMapping("/diss/findDissByUidRJX")
